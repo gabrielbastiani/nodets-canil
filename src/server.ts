1be908816d78e3bac9088d33c916a11bc40e7734
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-03-02 21:07:10 -0300

    'models'

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const server = express();
 
 server.set('view engine', 'mustache');
-server.set('views', path.join(__dirname, 'viewa'));
+server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
@@ -20,4 +20,6 @@ server.use((req, res)=>{
     res.send('página não encontrada!');
 });
 
-server.listen(process.env.PORT);
+server.listen(80);
+
+// process.env.PORT
